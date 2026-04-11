3eb0ea649a785f0e74fd4ac62035e2db5dbe8e76
WVista709 <massanitatikawa@gmail.com>
2026-04-11 19:33:22 -0400

    Atualizando dados do sql

---
+++ main/src/main/java/com/db/AtualizarDados.java
@@ -0,0 +1,32 @@
+package com.db;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+public class AtualizarDados {
+    public static void main(String[] args) {
+        Connection conn = null;
+        PreparedStatement pt = null;
+
+        try {
+            conn = DB.getConnection();
+
+            String linha = "UPDATE seller" 
+            + "SET BaseSalary + ?"
+            + "WHERE "
+            + "(DepartmentId = ?)";
+            pt = conn.prepareStatement(linha);
+            pt.setDouble(1, 200);
+            pt.setInt(2, 2);
+
+            int atualizarLinha = pt.executeUpdate();
+            System.out.println("Feito linhas afetadas: " + atualizarLinha);
+        } catch (Exception e) {
+            e.getMessage();
+        }
+        finally {
+            DB.closeConnection();
+            DB.closeStatement(pt);
+        }
+    }
+}
