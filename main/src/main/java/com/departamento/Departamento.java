2dc5f7166e4972ff43690a86b76d8a9ebd16dc8c
WVista709 <massanitatikawa@gmail.com>
2026-04-12 19:39:21 -0400

    Criando o pacote departamento e a classe Departamento

---
+++ main/src/main/java/com/departamento/Departamento.java
@@ -0,0 +1,57 @@
+package com.departamento;
+
+import java.io.Serializable;
+
+public class Departamento implements Serializable{
+    private static final long serialVersionUID = 1L;
+    private String nome;
+    private int id;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Departamento(String nome, int id) {
+        this.nome = nome;
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + id;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Departamento other = (Departamento) obj;
+        if (id != other.id)
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Departamento [nome=" + nome + ", id=" + id + "]";
+    }
+}
--- main/src/main/java/com/program/Main.java
+++ main/src/main/java/com/program/Main.java
@@ -3,10 +3,11 @@ package com.program;
 import java.sql.Connection;
 
 import com.db.DB;
+import com.departamento.Departamento;
 
 public class Main {
     public static void main(String[] args) {
-        Connection conn = DB.getConnection();
-        DB.closeConnection();
+        Departamento departamento = new Departamento("Livros", 1);
+        System.out.println(departamento);
     }
 }
